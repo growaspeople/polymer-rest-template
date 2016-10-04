672d2346a22e3a74988fc85dc5822dbd6c5739b7
Jumpei Ogawa <ogawa@growaspeople.org>
2016-10-04 20:29:48 +0900

    Run fetchJson on created, and create Polymer object on WebComponentsReady
    for performance improvement: ready is triggered just after all assets are loaded

--- rest-template.js
+++ rest-template.js
@@ -3,51 +3,53 @@
 (function() {
   "use strict";
 
-  Polymer({
-    is:         "rest-template",
-    properties: {
-      src: {
-        type:     String,
-        observer: "fetchJson"
-      },
-      engine: {
-        type:  String,
-        value: "handlebars"
-      },
-      data: {
-        type:  Object
-      }
-    },
-    ready: function() {
-      this.fetchJson();
-    },
-    getReceivedData: function() {
-      return this.data;
-    },
-    fetchJson: function() {
-      const self = this;
-
-      fetch(self.src).then(function(res) {
-        return res.json();
-      }).then(function(data) {
-        // TODO sort function
-
-        self.data = data;
-
-        if (!self.engine || self.engine === "handlebars") {
-          const template = Handlebars.compile(Polymer.dom(self).innerHTML);
-
-          Polymer.dom(self.root).innerHTML = template(data);
-        } else if (self.engine === "mustache") {
-          Polymer.dom(self.root).innerHTML = Mustache.render(Polymer.dom(self).innerHTML, data);
-        } else {
-          return Promise.reject(new Error("Unsupported template engine: " + self.engine));
+  window.addEventListener("WebComponentsReady", function() {
+    Polymer({
+      is:         "rest-template",
+      properties: {
+        src: {
+          type:     String,
+          observer: "fetchJson"
+        },
+        engine: {
+          type:  String,
+          value: "handlebars"
+        },
+        data: {
+          type:  Object
         }
+      },
+      created: function() {
+        this.fetchJson();
+      },
+      getReceivedData: function() {
+        return this.data;
+      },
+      fetchJson: function() {
+        const self = this;
 
-        self.fire("dataReceived");
-      }).catch(function(err) {
-        console.error(err);
-      });
-    }
+        fetch(self.src).then(function(res) {
+          return res.json();
+        }).then(function(data) {
+          // TODO sort function
+
+          self.data = data;
+
+          if (!self.engine || self.engine === "handlebars") {
+            const template = Handlebars.compile(Polymer.dom(self).innerHTML);
+
+            Polymer.dom(self.root).innerHTML = template(data);
+          } else if (self.engine === "mustache") {
+            Polymer.dom(self.root).innerHTML = Mustache.render(Polymer.dom(self).innerHTML, data);
+          } else {
+            return Promise.reject(new Error("Unsupported template engine: " + self.engine));
+          }
+
+          self.fire("dataReceived");
+        }).catch(function(err) {
+          console.error(err);
+        });
+      }
+    });
   });
 }());
