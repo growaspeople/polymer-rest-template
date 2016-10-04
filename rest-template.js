e91649bd4d5d4a153f4bf49e22deaa7373091cda
Jumpei Ogawa <ogawa@growaspeople.org>
2016-10-04 19:52:45 +0900

    Add getReceivedData function

--- rest-template.js
+++ rest-template.js
@@ -12,22 +12,30 @@
       engine: {
         type:  String,
         value: "handlebars"
+      },
+      data: {
+        type:  Object
       }
+    },
+    getReceivedData: function() {
+      return this.data;
     },
     ready: function() {
       const self = this;
 
       fetch(self.src).then(function(res) {
         return res.json();
-      }).then(function(json) {
+      }).then(function(data) {
         // TODO sort function
+
+        self.data = data;
 
         if (!self.engine || self.engine === "handlebars") {
           const template = Handlebars.compile(Polymer.dom(self).innerHTML);
 
-          Polymer.dom(self.root).innerHTML = template(json);
+          Polymer.dom(self.root).innerHTML = template(data);
         } else if (self.engine === "mustache") {
-          Polymer.dom(self.root).innerHTML = Mustache.render(Polymer.dom(self).innerHTML, json);
+          Polymer.dom(self.root).innerHTML = Mustache.render(Polymer.dom(self).innerHTML, data);
         } else {
           return Promise.reject(new Error("Unsupported template engine: " + self.engine));
         }
